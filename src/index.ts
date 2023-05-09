b1cbb558cf1ae98b1127d423483866edebd3844b
KwilLuke <105600466+KwilLuke@users.noreply.github.com>
2023-05-09 10:21:21 -0500

    exporting more types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import NodeKwil from './node/nodeKwil'
 import WebKwil from './web/webKwil'
-import { PayloadType } from './common/interfaces/tx'
-import { DataType, AttributeType, IndexType } from './common/interfaces/enums'
 import { generateDBID } from './utils/dbid'
 import { TxReceipt as _TxReceipt } from './common/interfaces/tx'
+import { Funder as _Funder } from './common/funder/funding'
+import { Action as _Action } from './common/action/action'
+import { DBBuilder as _DBBuilder } from './common/builder/builder'
 
 namespace Types {
     export type TxReceipt = _TxReceipt
+    export type Funder = _Funder
+    export type Action = _Action
+    export type DBBuilder = _DBBuilder
 }
 
 const Utils = {
