e33c450fadf27b1074ad5013023cea83d687d544
KwilLuke <luke@kwil.com>
2025-03-03 16:45:24 -0600

    ci: bump kwil-db version

--- test-eth-app/src/tests/testUtils.ts
+++ test-eth-app/src/tests/testUtils.ts
@@ -4,7 +4,7 @@ console.log(import.meta.env.VITE_KWIL_PROVIDER);
 console.log(import.meta.env.VITE_CHAIN_ID);
 
 export const kwil = new WebKwil({
-  kwilProvider: import.meta.env.VITE_KWIL_PROVIDER || 'http://localhost:8484',
+  kwilProvider: import.meta.env.VITE_KWIL_PROVIDER || 'http://localhost:8090',
   chainId: import.meta.env.VITE_CHAIN_ID || 'kwil-testnet',
   logging: true,
   timeout: 10000,
